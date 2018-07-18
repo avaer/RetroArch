53f788151e18ed6d8b2d8f0c8f77f4bf2627cfb1
Avaer Kazmer <a@modules.io>
2018-07-17 22:02:22 -0400

    Remove old hacks from build.sh

--- dist-scripts/build.sh
+++ dist-scripts/build.sh
@@ -4,8 +4,8 @@ source ~/emsdk/emsdk_env.sh
 export platform=emscripten
 export DEBUG=1
 emmake ./dist-cores.sh emscripten
-sed -i -e 's/case 0x8034 /case 0x8034: case 33638 /g' ../pkg/emscripten/*_libretro.js
-sed -i -e 's/RA\.process();/return 0;RA.process();/g' ../pkg/emscripten/*_libretro.js
-sed -i -e 's/RA\.process();/return 0;RA.process();/g' ../pkg/emscripten/*_libretro.js
-sed -i -e 's/nullFunc_iii(x) {/nullFunc_iii(x) { return 0;/g' ../pkg/emscripten/*_libretro.js
+# sed -i -e 's/case 0x8034 /case 0x8034: case 33638 /g' ../pkg/emscripten/*_libretro.js
+# sed -i -e 's/RA\.process();/return 0;RA.process();/g' ../pkg/emscripten/*_libretro.js
+# sed -i -e 's/RA\.process();/return 0;RA.process();/g' ../pkg/emscripten/*_libretro.js
+# sed -i -e 's/nullFunc_iii(x) {/nullFunc_iii(x) { return 0;/g' ../pkg/emscripten/*_libretro.js
 cp ../pkg/emscripten/*_libretro.{js,wasm} ~/emukit/assets/frontend/bundle/
